a1fc6733aa8a2d633a76a44796ca9bb951a1d005
Yu yang <907016263@qq.com>
2019-12-07 02:41:06 -0800

    Add grid and Gems, will finish it as soon as possible, pls dont mark it yet tx

---
+++ Assets/Resources/Scripts/Check.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Check : MonoBehaviour {
+
+    public GameObject[] mGems = new GameObject[6];
+    public GameObject CreateGem()
+    {
+        int num = Random.Range(0, 5);
+        GameObject gem = Instantiate(mGems[num]);
+        return gem;
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.childCount==0)
+        {
+            GameObject sonGems = CreateGem();
+            sonGems.transform.parent=transform;
+            sonGems.transform.position = transform.position;
+
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject mGrid;
+    public int mx = 5;
+    public int my = 8;
+    private static GameManager _instance;
+    public static GameManager instance
+    {
+        get
+        {
+            return _instance;
+        }
+
+        set
+        {
+            _instance = value;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+        for (int i = 0; i < mx; i++)
+        {
+            for (int j = 0; j < my; j++)
+            {
+                GameObject box = Instantiate(mGrid);
+                box.transform.position = Correct(i, j);
+                box.transform.SetParent(transform);
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public Vector3 Correct(int x, int y)
+    {
+        return new Vector3(transform.position.x - mx / 2f + x, transform.position.y + my / 2f - y, 0);
+    }
+}
+
